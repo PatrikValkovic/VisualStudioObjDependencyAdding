f127a6191e35be1e740ea2a6f64c9ae75a4cc3f4
Patrik Valkovic <pata.va@seznam.cz>
2014-10-21 21:12:30 +0200

    First commit

---
+++ ObjFileAdding/MainMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+
+namespace ObjFileAdding
+{
+    class MainMethod
+    {
+        static string Source = Properties.Resources.SourceFolder + "\\PacmanUnitTests.vcxproj";
+        #if DEBUG
+                static string Destination = Properties.Resources.DestinationFolderDebug;
+        #else 
+                static string Destination = Properties.Resources.DestinationFolderRelease; 
+        #endif
+
+
+
+        public static void Main()
+        {
+            List<string> files=Methods.GetFilesNames(Destination);
+
+            string TextToAdd=Methods.createTextInside(files);
+
+            Methods.ChangeXML(TextToAdd,Source);
+
+            return;
+        }
+    }
+}
---
+++ ObjFileAdding/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace ObjFileAdding
+{
+    class Methods
+    {
+        public static List<string> GetFilesNames(string Destination)
+        {
+            string[] files = Directory.GetFiles(Destination, "*.obj", SearchOption.AllDirectories);
+            List<string> NameOfFiles = new List<string>(files.Length);
+            foreach (string file in files)
+                NameOfFiles.Add(Path.GetFileName(file));
+            return NameOfFiles;
+        }
+
+
+        public static string createTextInside(List<string> files)
+        {
+            StringBuilder builder = new StringBuilder("SDL2.lib;SDL2main.lib;SDL2test.lib;");
+            foreach (string file in files)
+                builder.Append(file).Append(";");
+            return builder.ToString();
+        }
+
+
+        public static void ChangeXML(string TextToAdd, string Source)
+        {
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(File.ReadAllText(Source));
+            XmlNodeList Link = document.GetElementsByTagName("AdditionalDependencies");
+            foreach (XmlNode node in Link)
+            {
+                node.InnerText = TextToAdd;
+            }
+            document.Save(Source);
+        }
+    }
+}
